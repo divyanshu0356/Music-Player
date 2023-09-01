cb4c2168e1f402c937fc80cc1848bb785ab9d60d
Divyanshu Kumar <134629130+divyanshu0356@users.noreply.github.com>
2023-09-01 17:22:48 +0530

    Create Song.java

---
+++ Song.java
@@ -0,0 +1,24 @@
+package div;
+
+public class Song {
+  String title;
+  double duration;
+  public Song(String title,double duration) {
+	  this.title=title;
+	  this.duration=duration;
+  }
+  public Song() {
+	  
+  }
+  public String getTitle() {
+	  return title;
+  }
+  public double getDuration() {
+	  return duration;
+  }
+  public String toString() {
+	  return "Song{" +"title=" + title +'\''+
+			  ", duration=" + duration + '}';
+  }
+  
+}
